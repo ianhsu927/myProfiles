47a566cd109e7ae957e8946c14d54c65a7cdef3c
Ian Hsu <ian927@qq.com>
2024-02-22 10:56:11 +0800

    增加 clash 检测

--- linux.sh
+++ linux.sh
@@ -9,3 +9,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+if pgrep -x "clash" >/dev/null; then
+    echo "Clash is running. Setting up https_proxy."
+    export https_proxy="http://127.0.0.1:7893" # 请根据您的clash代理设置调整端口号
+    export http_proxy="http://127.0.0.1:7893"
+else
+    echo "Clash is not running."
+fi
